75a60ed8aa90aac13cbb1080607a2351781fbd84
kishan maurya <kishanmaurya@kishans-MacBook-Air.local>
2026-05-09 02:12:41 +0530

    Prepare app for Render deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 class CircularQueue {
     constructor(capacity, label) {
